9960cac689555fd4dc95f304ca1d28eef1a0e0bd
yan-Rab <yrabelo@morphus.com.br>
2021-09-17 10:50:59 -0300

    Finalizando api school

---
+++ school/serializers.py
@@ -0,0 +1,56 @@
+# Third party
+from rest_framework import serializers
+
+# Local
+from .models import Student
+from .models import Course
+from .models import Registration
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'rg', 'cpf', 'birth_date']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['id', 'code', 'name', 'description', 'level']
+
+
+class RegistrationSerializerList(serializers.ModelSerializer):
+    student = StudentSerializer()
+    course = CourseSerializer()
+
+    class Meta:
+        model = Registration
+        fields = ['id', 'student', 'course', 'time_course']
+
+
+class RegistrationSerializerCreateAndUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = ['id', 'student', 'course', 'time_course']
+
+
+class RegistrationsByStudentSerializer(serializers.ModelSerializer):
+    course = serializers.CharField(source='course.name')
+    time_course = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Registration
+        fields = ['course', 'time_course']
+
+
+    def get_time_course(self, obj):
+        # return TIME_COURSE_VALUES[obj.time_course][1]
+        return obj.get_time_course_display()
+
+
+class StudentsByCourseSerializer(serializers.ModelSerializer):
+    student = serializers.CharField(source='student.name')
+
+    class Meta:
+        model = Registration
+        fields = ['student']
---
+++ school/views.py
@@ -0,0 +1,67 @@
+# Third party
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+# Local
+from .models import Student
+from .models import Course
+from .models import Registration
+from .serializers import StudentSerializer
+from .serializers import CourseSerializer
+from .serializers import RegistrationSerializerList
+from .serializers import RegistrationSerializerCreateAndUpdate
+from .serializers import RegistrationsByStudentSerializer
+from .serializers import StudentsByCourseSerializer
+
+
+class StudentsViewset(viewsets.ModelViewSet):
+    """Exibindo todos os estudantes"""
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class CoursesViewset(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class RegistrationsViewset(viewsets.ModelViewSet):
+    """Exibindo matrículas"""
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializerCreateAndUpdate
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return RegistrationSerializerList
+        return super().get_serializer_class()
+
+
+class RegistrationsByStudent(ListAPIView):
+    """Exibindo matrículas de um aluno"""
+    serializer_class = RegistrationsByStudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Registration.objects.filter(student=self.kwargs['pk'])
+        return queryset
+
+
+class StudentsByCourse(ListAPIView):
+    """Exibindo alunos de um curso"""
+    serializer_class = StudentsByCourseSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Registration.objects.filter(course=self.kwargs['pk'])
+        return queryset
